407bc075e6d6ca6fd18f80221b0ffd292df4cce4
brullyz <brullyz01@gmail.com>
2025-06-18 15:58:29 +0700

    fix internationalization

--- commons/src/main/java/com/project/commons/configuration/MessageSourceConfig.java
+++ commons/src/main/java/com/project/commons/configuration/MessageSourceConfig.java
@@ -1,21 +1,36 @@
 package com.project.commons.configuration;
 
-import org.springframework.context.MessageSource;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.server.i18n.AcceptHeaderLocaleContextResolver;
+import org.springframework.web.server.i18n.LocaleContextResolver;
+
+import java.util.Locale;
 
 /**
  * Created by user on 8:42 18/05/2025, 2025
  */
 @Configuration
 public class MessageSourceConfig {
+
     @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setUseCodeAsDefaultMessage(true);
-        return messageSource;
+    public ResourceBundleMessageSource messageSource() {
+        var resourceBundleMessageSource=new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasenames("messages"); // directory with messages_XX.properties
+        resourceBundleMessageSource.setUseCodeAsDefaultMessage(true);
+        resourceBundleMessageSource.setDefaultLocale(Locale.of("en"));
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        resourceBundleMessageSource.setAlwaysUseMessageFormat(true);
+        return resourceBundleMessageSource;
     }
+
+    @Bean
+    public LocaleContextResolver localeResolver() {
+        AcceptHeaderLocaleContextResolver resolver = new AcceptHeaderLocaleContextResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH); // fallback
+        return resolver;
+    }
+
 }
--- commons/src/main/java/com/project/commons/controller/BaseController.java
+++ commons/src/main/java/com/project/commons/controller/BaseController.java
@@ -1,28 +1,26 @@
 package com.project.commons.controller;
 
 
-import com.project.commons.model.enums.MethodMessage;
-import com.project.commons.model.enums.StatusConstant;
 import com.project.commons.model.response.ObjectApiResponse;
 import com.project.commons.model.response.ObjectPagedApiResponse;
-import com.project.commons.utils.ResourceBundle;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
+import com.project.commons.utils.MessageUtil;
 import org.springframework.data.domain.Page;
-import org.springframework.util.StringUtils;
 
-@RequiredArgsConstructor
 public class BaseController {
 
-	private final ResourceBundle resourceBundle;
+	protected String getDefaultStatusSuccessMessage() {
+		return MessageUtil.getMessage("msg.success");
+	}
+
+	protected String getDefaultSuccessMessage() {
+		return MessageUtil.getMessage("msg.data.get.success");
+	}
 	
 	public ObjectApiResponse responseApi(Object data) {
 
 		ObjectApiResponse dto = new ObjectApiResponse();
-		if (StringUtils.hasText(dto.getStatus()) || StringUtils.hasText(dto.getMessage())) {
-			dto.setStatus(StatusConstant.SUCCESS.getEn());
-			dto.setMessage(resourceBundle.getMessage("msg.data.get.success"));
-		}
+		dto.setStatus(getDefaultStatusSuccessMessage());
+		dto.setMessage(getDefaultSuccessMessage());
 		dto.setData(data);
 		
 		return dto;
@@ -31,10 +29,8 @@ public class BaseController {
 	public <T> ObjectPagedApiResponse responseApiPaged(Page<T> page) {
 
 		ObjectPagedApiResponse dto = new ObjectPagedApiResponse();
-		if (StringUtils.hasText(dto.getStatus()) || StringUtils.hasText(dto.getMessage())) {
-			dto.setStatus(StatusConstant.SUCCESS.getEn());
-			dto.setMessage(resourceBundle.getMessage("msg.data.get.success"));
-		}
+		dto.setStatus(getDefaultStatusSuccessMessage());
+		dto.setMessage(getDefaultSuccessMessage());
 		dto.setData(page.getContent());
 		dto.setPage(page.getNumber());
 		dto.setTotalElement(page.getTotalElements());
